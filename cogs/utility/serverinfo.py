d2083f09afa4faef4c796465387291351c570616
spiyse <daniels@spiyse.dev>
2026-02-01 00:56:25 +0200

    added server info and bot uptime commands

---
+++ cogs/utility/serverinfo.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import discord
+
+
+class ServerInfoCommand(commands.Cog):
+    @commands.command(name="serverinfo", aliases=["si"])
+    async def serverinfo(self, ctx: commands.Context) -> None:
+        # description for the command
+        """Shows the server's general info."""
+
+        embed = discord.Embed(
+            title="Server Info",
+            description="\nGeneral info about the server :)\n",
+            colour=0x00F5A3,
+            timestamp=discord.utils.utcnow(),
+        )
+
+
+        embed.set_author(
+            name="Glissm",
+            icon_url="https://cdn.discordapp.com/app-icons/1465776416707579986/4f3347b58711ce9d9291982e36ee0f0c.png?size=512&quot;);",
+        )
+
+        embed.add_field(name="Server ID", value=f"`{ctx.guild.id}`", inline=True)
+        embed.add_field(name="Server Name", value=f"`{ctx.guild.name}`", inline=True)
+        embed.add_field(name="Created on", value=f"`{ctx.guild.created_at}`", inline=True)
+        embed.add_field(name="Region", value=f"`{ctx.guild.preferred_locale}`", inline=True)
+        embed.add_field(name="Members", value=f"`{ctx.guild.member_count}`", inline=True)
+        embed.add_field(name="Owner", value=f"{ctx.guild.owner.mention}", inline=True)
+
+        embed.set_footer(text="Hi")
+
+        await ctx.send(embed=embed)
---
+++ cogs/utility/uptime.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+import discord
+import datetime, time
+
+class UptimeCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.startTime = time.time()
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.startTime = time.time()
+    
+    @commands.command(name="uptime")
+    async def ping(self, ctx: commands.Context) -> None:
+        # description for the command
+        """Shows the bots uptime."""
+        
+        uptime = str(datetime.timedelta(seconds=int(round(time.time()-self.startTime))))
+        
+        embed = discord.Embed(
+            title="Uptime",
+            description=f"\n```\n{uptime}\n```",
+            timestamp=discord.utils.utcnow()
+            )
+
+        embed.set_footer(text="Requested")
+        
+        await ctx.send(embed=embed)
+            
+            
