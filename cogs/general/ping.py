83ffd3bec818d9c72b7213642b9d2510dbdeb6f6
spiyse <daniels@spiyse.dev>
2026-02-02 00:50:12 +0200

    ping gets shown in a embed now

--- cogs/general/ping.py
+++ cogs/general/ping.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-
+import discord
 
 class PingCommand(commands.Cog):
     @commands.command(name="ping")
@@ -8,4 +8,13 @@ class PingCommand(commands.Cog):
         """Shows the bots ping."""
         
         latency_ms = round(self.bot.latency * 1000)
-        await ctx.send(f"Pong! Latency: **{latency_ms}** ms.")
+
+        embed = discord.Embed(
+            title="Latency",
+            description=f"\n```\n{latency_ms} ms\n```",
+            timestamp=discord.utils.utcnow()
+            )
+
+        embed.set_footer(text="Requested")
+        
+        await ctx.send(embed=embed)
